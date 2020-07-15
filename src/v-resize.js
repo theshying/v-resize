6cb2d46c8c39f5707ed8db7756805d92a28288a9
许尚余 <xushangyu@rlair.net>
2020-07-15 16:41:27 +0800

    fix: 修复判断是否兼容ResizeObserver逻辑

--- src/v-resize.js
+++ src/v-resize.js
@@ -70,7 +70,7 @@ const emitElementSize = () => {
 
 //是否支持原生resize observer
 const supportResizeObserver = () => {
-    return !isFunction(ResizeObserver)
+    return window.ResizeObserver && isFunction(window.ResizeObserver)
 }
 //是否是函数
 const isFunction = v => Object.prototype.toString.call(v) === '[object Function]'
